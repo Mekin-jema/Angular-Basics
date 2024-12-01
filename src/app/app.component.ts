91aba83a2a2f065f834b89937e1f1410f8edd54b
Mekin Jemal <mekinjemal999@gmail.com>
2024-12-01 09:09:00 +0300

    My First angular project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './Common/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    HomeComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
